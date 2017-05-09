ad9f440a0b4d6c8d8ca8d0b40388e73d7e70afca
ktsn <ktsn55@gmail.com>
2017-05-10 00:05:23 +0900

    Add `spec` suffix for test cases

--- scripts/webpack.config.test.js
+++ scripts/webpack.config.test.js
@@ -2,7 +2,7 @@ const path = require('path')
 const glob = require('glob')
 
 module.exports = {
-  entry: glob.sync(path.resolve(__dirname, '../test/**/*.ts')),
+  entry: glob.sync(path.resolve(__dirname, '../test/**/*.spec.ts')),
   output: {
     path: path.resolve(__dirname, '../.tmp'),
     filename: 'test.js'
